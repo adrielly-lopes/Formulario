cf8b0bfb3a52705a0c6da6766e53c71099c29a43
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-17 12:00:42 -0400

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,26 +1,11 @@
-<?php include_once"conexao.php";?>
-<html>
-<body>
 <?php
-$nome = $_GET["nome"];
-$telefone = $_GET["telefone"];
-$email = $_GET["email"];
-$formacao = $_GET["nivel"];
-$area = $_GET["formacao"];
-$outros = $_GET["experiencia"];
+include_once "conexao.php";
 
-$conn= mysqli_connect($host, $usuario, $senha, $bd);
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$formacao = $_POST['nivel'];
+$area = $_POST['formacao'];
+$outros = $_POST['outra_area'];
 
-mysqli_select_db($conn, '$bd');
-$sql = "INSERT INTO usuarios (nome, telefone, email, formacao, area, outros) VALUES ('$nome', '$telefone', '$email', '$formacao', '$area', '$outros')";
-
-if(mysqli_query($conn, $sql)){
-    echo("<script>alert('Salvei seus dados!'); window.location = 'formulario.html';</script>");
-}
-else{
-    echo("Erro na conexão".$sql."<br>".mysqli_error($conn));
-}
-mysqli_close($conn);
 ?>
-</body>
-</html>
