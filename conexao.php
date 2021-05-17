a21461cba149517d747203a013ff8df093b35bf9
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-17 12:02:27 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,26 @@
 <?php
 
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$bd = "cadastro";
-$conn= mysqli_connect($host, $usuario, $senha, $bd);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "cadastro";
+
+$conn= mysqli_connect($servername, $username, $password, $database);
 
 if(!$conn){
     die("Erro de conexão: ".mysqli_connect_error());
 }
+echo "Conexão bem sucedida";
+
+mysqli_select_db($conn, '$database');
+$sql = "INSERT INTO usuarios (nome, telefone, email, formacao, area, outros) VALUES ('$nome', '$telefone', '$email', '$formacao', '$area', '$outros')";
+
+if(mysqli_query($conn, $sql)){
+    echo "Salvei seus dados";
+}
+else{
+    echo "Erro na conexão" .$sql. "<br>". mysqli_error($conn);
+}
+mysqli_close($conn);
+
 ?> 
