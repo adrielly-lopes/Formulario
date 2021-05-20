f1ab90665ab0321e40f2b46d0e2ec7cbd25c68a4
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-20 12:54:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
     
     <div>
         <h1 id="titulo">Cadastro</h1>
-        <h1 id="subtitulo">EGOV</h1>
+        <h1 id="subtitulo">Escola de Governo de Corumbá</h1>
+        <img id="imagem" src="logo-egov.png">
         <br>
     </div>
-
+    
     <form action="recebe.php" method="POST"> 
         <fieldset class="grupo">
             <div class="campo">
@@ -22,6 +23,11 @@
                 <input type="text" name="nome" id="nome" required>
             </div>
 
+            <div class="campo">
+                <label for="cpf">CPF</label>
+                <input type="text" name="cpf" id="cpf" required>
+            </div>
+            
             <div class="campo">
                 <label for="telefone">Telefone</label>
                 <input type="text" name="telefone" id="telefone" required>
@@ -34,59 +40,67 @@
         </fieldset>
 
             <div class="campo">
-                <label>Formação:</label>
+                <label>Qual é o seu grau acadêmico?</label>
                 <label>
-                    <input type="radio" name="nivel" value="graduacao">Graduação
+                    <input type="radio" name="formacao" value="Graduacao">Graduação
                 </label>
                 <label>
-                    <input type="radio" name="nivel" value="pos-graduacao">Pós-Graduação
+                    <input type="radio" name="formacao" value="Pos-graduacao">Pós-Graduação
                 </label>
                 <label>
-                    <input type="radio" name="nivel" value="mestrado">Mestrado
+                    <input type="radio" name="formacao" value="Mestrado">Mestrado
                 </label>
                 <label>
-                    <input type="radio" name="nivel" value="doutorado">Doutorado
+                    <input type="radio" name="formacao" value="Doutorado">Doutorado
                 </label>
             </div>
 
         <fieldset class="grupo">
             <div id="check">
-                <label>Área de formação:</label><br><br>
-                <input type="checkbox" id="formacao" name="formacao" value="Sistemas">
-                <label for="formacao1">Sistemas de Informação</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Educação">
-                <label for="formacao2">Educação Física</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Administracao">
-                <label for="formacao3">Administração</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Contabilidade">
-                <label for="formacao4">Ciências Contábeis</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Geografia">
-                <label for="formacao5">Geografia</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Historia">
-                <label for="formacao6">História</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Biologia">
-                <label for="formacao7">Ciências Biológicas</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Pisicologia">
-                <label for="formacao8">Pisicologia</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Direitos">
-                <label for="formacao9">Direitos</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Ingles">
-                <label for="formacao10">Letras Português/Inglês</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Espanhol">
-                <label for="formacao11">Letras Português/Espanhol</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Matematica">
-                <label for="formacao12">Matemática</label>
-                <input type="checkbox" id="formacao" name="formacao" value="Pedagogia">
-                <label for="formacao13">Pedagogia</label>
+                <label>Qual é a sua área de formação?</label><br><br>
+                <input type="checkbox" id="formacao1" name="area" value="Administração, Economia e Negócios">
+                <label for="formacao1">Administração, Economia e Negócios</label>
+                <input type="checkbox" id="formacao2" name="area" value="Ciências Sociais">
+                <label for="formacao2">Ciências Sociais</label>
+                <input type="checkbox" id="formacao3" name="area" value="Comunicação e Mídia">
+                <label for="formacao3">Comunicação e Mídia</label>
+                <input type="checkbox" id="formacao4" name="area" value="Design e Arte">
+                <label for="formacao4">Design e Arte</label>
+                <input type="checkbox" id="formacao5" name="area" value="Direito">
+                <label for="formacao5">Direito</label>
+                <input type="checkbox" id="formacao6" name="area" value="Educação">
+                <label for="formacao6">Educação</label>
+                <input type="checkbox" id="formacao7" name="area" value="Engenharia e Arquitetura">
+                <label for="formacao7">Engenharia e Arquitetura</label>
+                <input type="checkbox" id="formacao8" name="area" value="Informação e Tecnologia">
+                <label for="formacao8">Informação e Tecnologia</label>
+                <input type="checkbox" id="formacao9" name="area" value="Saúde">
+                <label for="formacao9">Saúde</label>
+                <input type="checkbox" id="formacao10" name="area" value="Turismo e Gastronomia">
+                <label for="formacao10">Turismo e Gastronomia</label>
+                <input type="checkbox" id="formacao11" name="area" value="Ciências Exatas e da Terra">
+                <label for="formacao11">Ciências Exatas e da Terra</label>
+                <input type="checkbox" id="formacao12" name="area" value="Ciências Biológicas">
+                <label for="formacao12">Ciências Biológicas</label>
+                <input type="checkbox" id="formacao13" name="area" value="Ciências Humanas">
+                <label for="formacao13">Ciências Humanas</label>
+                <input type="checkbox" id="formacao14" name="area" value="Letras e Linguística">
+                <label for="formacao14">Letras e Linguística</label>
             </div>
         </fieldset>
 
         <div class="campo">
             <br>
-            <label for="outra_area">Outras áreas de formação: </label>
-            <textarea rows="6" style="width: 26em" id="outra_area" name="outra_area"></textarea>
+            <label for="outras_areas">Possuí outras áreas de formação além da selecionada?</label>
+            <textarea rows="6" style="width: 26em" id="outras_areas" name="outras_areas"></textarea>
         </div>
-
+        
+        <div class="campo">
+            <br>
+            <label for="curso">Qual(is) seu(s) curso(s)?</label>
+            <textarea rows="6" style="width: 26em" id="curso" name="curso"></textarea>
+        </div>
+        
         <button class="botao" type="submit" onsubmit="">Cadastrar</button>
 
     </form>
