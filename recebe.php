28ef4f589db8f8a5b4e29beb450575b6add923ce
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-20 12:55:04 -0400

    Update recebe.php

--- recebe.php
+++ recebe.php
@@ -2,14 +2,16 @@
 include_once('conexao.php');
 
 $nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
 $telefone = $_POST['telefone'];
 $email = $_POST['email'];
-$formacao = $_POST['nivel'];
-$area = $_POST['formacao'];
-$outros = $_POST['outra_area'];
+$formacao = $_POST['formacao'];
+$area = $_POST['area'];
+$outras_areas = $_POST['outras_areas'];
+$curso = $_POST['curso'];
 
 mysqli_select_db($conn, '$database');
-$sql = "INSERT INTO usuarios (nome, telefone, email, formacao, area, outros) VALUES ('$nome', '$telefone', '$email', '$formacao', '$area', '$outros')";
+$sql = "INSERT INTO tabela_usuarios (nome, cpf, telefone, email, formacao, area, outras_areas, curso) VALUES ('$nome', '$cpf', '$telefone', '$email', '$formacao', '$area', '$outras_areas', '$curso')";
 
 if(mysqli_query($conn, $sql)){
     echo "Salvei seus dados!";
@@ -18,4 +20,3 @@ else{
     echo "Ops! Você já fez seu cadastro!";
 }
 ?>
-
