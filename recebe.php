12465db14d214c1475b24762b2cb1e54df6d8967
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-21 09:39:51 -0400

    Update recebe.php

--- recebe.php
+++ recebe.php
@@ -11,7 +11,7 @@ $outras_areas = $_POST['outras_areas'];
 $curso = $_POST['curso'];
 
 mysqli_select_db($conn, '$database');
-$sql = "INSERT INTO tabela_usuarios (nome, cpf, telefone, email, formacao, area, outras_areas, curso) VALUES ('$nome', '$cpf', '$telefone', '$email', '$formacao', '$area', '$outras_areas', '$curso')";
+$sql = "INSERT INTO candidatos (nome, cpf, telefone, email, formacao, area, outras_areas, curso) VALUES ('$nome', '$cpf', '$telefone', '$email', '$formacao', '$area', '$outras_areas', '$curso')";
 
 if(mysqli_query($conn, $sql)){
     echo "Salvei seus dados!";
