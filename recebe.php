c090e9ad67918936b466445e9280caa3cfaaffb9
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-05-24 13:04:06 -0400

    Update recebe.php

--- recebe.php
+++ recebe.php
@@ -5,13 +5,18 @@ $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 $telefone = $_POST['telefone'];
 $email = $_POST['email'];
+$endereco = $_POST['endereco'];
+$bairro = $_POST['bairro'];
+$cep = $_POST['cep'];
+$data_nascimento = $_POST['data_nascimento'];
+$sexo = $_POST['sexo'];
 $formacao = $_POST['formacao'];
 $area = $_POST['area'];
-$outras_areas = $_POST['outras_areas'];
 $curso = $_POST['curso'];
+$termo = $_POST['termo'];
 
 mysqli_select_db($conn, '$database');
-$sql = "INSERT INTO candidatos (nome, cpf, telefone, email, formacao, area, outras_areas, curso) VALUES ('$nome', '$cpf', '$telefone', '$email', '$formacao', '$area', '$outras_areas', '$curso')";
+$sql = "INSERT INTO cadastro (nome, cpf, telefone, email, endereco, bairro, cep, data_nascimento, sexo, formacao, area, curso, termo) VALUES ('$nome', '$cpf', '$telefone', '$email', '$endereco', '$bairro', $cep', '$data_nascimento, '$sexo', '$formacao', '$area', '$curso', '$termo)";
 
 if(mysqli_query($conn, $sql)){
     echo "Salvei seus dados!";
